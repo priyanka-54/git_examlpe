ad7a28c5ec86a44826271704f8865488313daf72
priyanka kadu <priyanka kadu@DESKTOP-BT2HK2Q>
2020-01-08 13:56:31 +0530

    hello and welcome

--- ParallelExecutionUsingXmlFile/src/ParrallelExecution2/Fiplkart.java
+++ ParallelExecutionUsingXmlFile/src/ParrallelExecution2/Fiplkart.java
@@ -12,7 +12,7 @@ public class Fiplkart {
       WebDriver driver=new FirefoxDriver();
       String Url="https://www.Flipkart.com/";
       driver.get(Url);
-      System.out.println("hello java");
+      System.out.println("hello java w");
   }
   
 }
